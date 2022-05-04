2fe36d0736afa2b8e452587230c35ebae195bd9a
felipe-cd-melo <felipe.cd.melo@gmail.com>
2022-05-04 01:56:11 -0300

    adicionados metodos a classe Estoque - checarProduto, getProduto, aumentaEstoque

--- src/com/letscode/Estoque.java
+++ src/com/letscode/Estoque.java
@@ -4,13 +4,37 @@ public class Estoque {
 
     private Object[][] estoque = new Object[10][2]; //colunas: 0-produto 1-identificadorFinal
 
-    //checar se o identificadorFinal ja existe no estoque
+    public boolean checarProduto(String identificadorFinal){
+        for(Object[] produtos: this.estoque){
+            if(produtos[1].equals(identificadorFinal)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Produto getProduto(String identificadorFinal){
+        for(Object[] produtos: this.estoque){
+            if(produtos[1].equals(identificadorFinal)){
+                return (Produto) produtos[0];
+            }
+        }
+        return null;
+    }
+
+    public void aumentaEstoque(){
+        Object[][] estoqueNovo = new Object[(this.estoque.length + 20)][2];
+        for(int i = 0; i < this.estoque.length; i++){
+            estoqueNovo[i] = this.estoque[i];
+            }
+        this.estoque = estoqueNovo;
+        }
+
+    }
+    //TODO:
     //adicionar o produto
     //adicionar a quantidade
 
-    //aumentar a matriz quando chegar no limite
-
-    //retornar o produto
     //printar o esqtoque
     // listar produtos por tipo
 
